e5252e356f7ce28bae47e75a39921968e0a6ca73
QuietClickCode <2572417548@qq.com>
2019-07-13 12:49:19 +0800

    添加ignore文件

---
+++ src/main/java/com/nbclass/SpringbootApplication.java
@@ -0,0 +1,21 @@
+package com.nbclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @version V1.0
+ * @date 2018年7月11日
+ * @author superzheng
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.nbclass.mapper")
+public class SpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
